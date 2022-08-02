dd9919d0b858dfa0868b66aaa08e4b09764abdab
Игорь Наточий <igornatochiy96@mail.ru>
2022-08-03 00:54:50 +0600

    Добавьте файлы проекта.

---
+++ Задача 16.1/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_16._1
+{
+    class Product
+    {
+        public int Num { get; set; }
+        public string Name { get; set; }
+        public float Summa  { get; set; }
+    }
+}
