580bf537715c167b8600ab3df3c959560c5e5990
IgorNatochiy <108982602+IgorNatochiy@users.noreply.github.com>
2022-08-03 21:35:13 +0600

    Add files via upload

---
+++ Задача 17/Products.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_17
+{
+    class Product <T>
+    {
+        public T Num { get; set; }
+        public string Name { get; set; }
+        public float Summa  { get; set; }
+
+        //public Product(T num, string name, int summa)
+        //{
+        //    Num = num;
+        //    Name = name;
+        //    Summa = summa;
+        //}
+
+        public string GetInfo()
+        {
+            return $"{Num} {Name} {Summa}";
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            Summa = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Задача 17/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите номер счёта");
+            //int numInt = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите ФИО");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Введите баланс");
+            //int summa = Convert.ToInt32(Console.ReadLine());
+
+            //Product<int> product1 = new Product<int>(numInt, name, summa);
+            //Console.WriteLine(product1.GetInfo());
+
+            //Console.WriteLine("Введите строковый номер счёта");
+            //string numString = Console.ReadLine();
+            //Console.WriteLine("Введите ФИО");
+            //name = Console.ReadLine();
+            //Console.WriteLine("Введите баланс");
+            //summa = Convert.ToInt32(Console.ReadLine());
+            //Product<string> product2 = new Product<string>(numString, name, summa);
+
+            //Console.WriteLine(product1.GetInfo());
+
+            Product<int> product1 = new Product<int>();
+            product1.Input();
+            Console.WriteLine(product1.GetInfo());
+            
+            Product<string> product2 = new Product<string>();
+            product2.Input();
+            Console.WriteLine(product2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
